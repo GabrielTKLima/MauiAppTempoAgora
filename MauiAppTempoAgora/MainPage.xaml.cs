3e0edb47f484f1be469ad135c99e7d9f2c6b737b
GabrielTKLima <tevudaz@gmail.com>
2026-09-20 21:53:15 -0300

    Atualização com a informação de Velocidade do vento e inclusão textual da velocidade (km/h) e Visibilidade (m "metros")

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -27,8 +27,8 @@ namespace MauiAppTempoAgora
                         dados_previsao = $"Latitude: {t.lat} \n" +
                                          $"Longitude: {t.lon} \n" +
                                          $"Descrição: {t.description} \n" +
-                                         $"Condição: {t.main} \n" +
-                                         $"Visibilidade: {t.visibility} \n" +
+                                         $"Velocidade do vento: {t.speed} km/h \n" +
+                                         $"Visibilidade: {t.visibility} m \n" +
                                          $"Nascer do Sol: {t.sunrise} \n" +
                                          $"Por do Sol: {t.sunset} \n" +
                                          $"Temp Máx: {t.temp_max} \n" +
